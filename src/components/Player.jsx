523536e13bcb4a9647d5bb7ea2afe3b9c26720e9
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-03-24 18:03:55 +0300

    my realization of useState function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+    const [ isEditing, setIsEditing ] = useState(false);
+    
+    function handleSelect() {
+        setIsEditing(true);
+    }
+
     return (
         <li>
           <span className='player'>
             <span className='player-name'>{name}</span>
             <span className='player-symbol'>{symbol}</span>
           </span>
-          <button>Edit</button>
-          </li>
+            {!isEditing ? (
+                <button onClick={() => handleSelect()}>Edit</button>
+            ) : <input></input>}
+        </li>
     )
 }
