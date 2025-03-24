dc6fa6a9de7ba892b902a58bafc282aa5457fdfe
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-03-24 18:13:26 +0300

    Remastered useState integration

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,19 +3,23 @@ import { useState } from 'react';
 export default function Player({ name, symbol }) {
     const [ isEditing, setIsEditing ] = useState(false);
     
-    function handleSelect() {
+    function handleEditClick() {
         setIsEditing(true);
+    }
+
+    let playerName = <span className='player-name'>{name}</span>;
+
+    if (isEditing) {
+        playerName = <input type='text' required />;
     }
 
     return (
         <li>
-          <span className='player'>
-            <span className='player-name'>{name}</span>
-            <span className='player-symbol'>{symbol}</span>
-          </span>
-            {!isEditing ? (
-                <button onClick={() => handleSelect()}>Edit</button>
-            ) : <input></input>}
+            <span className='player'>
+                {playerName}
+                <span className='player-symbol'>{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
